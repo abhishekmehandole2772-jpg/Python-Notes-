7537ff4a14f3c0df9e87fae8af196aae42e7f869
Abhishek Mehandole <abhishekmehandole2772@gmail.com>
2025-12-12 13:32:22 +0530

    first upload of new folder

---
+++ New folder/python vs code/loop.py
@@ -0,0 +1,18 @@
+#for i in range (5):
+#    print(i)
+
+#for i in range (5):
+   # print(i+1)
+
+#for k in range (1,10):
+   # print(k)
+
+#for k in range (1,10,2):
+   # print(k)
+
+#for i in range (1,10):
+ #   print(i+1)
+
+for i in range (1,6):
+    print(i,"squard is ",i+i)
+    
